59b5964fe06e3c05ea94317cf1bc7198a45bdaaf
Jaakko Keränen <jaakko.keranen@iki.fi>
2015-07-09 11:04:44 +0300

    MD5: Multiple animations specified via config property
    Added a configuration property that specifies a list of animation
    sequence names. The MD5 loader will then load each of these and they
    become separate animations for the model.
    
    If the property is omitted, the default behavior is the same as
    before: load the md5anim file that matches the md5mesh file name.

--- code/MD5Loader.h
+++ code/MD5Loader.h
@@ -53,6 +53,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 struct aiNode;
 struct aiNodeAnim;
+struct aiAnimation;
 
 namespace Assimp    {
 
@@ -111,7 +112,12 @@ protected:
     // -------------------------------------------------------------------
     /** Load a *.MD5ANIM file.
      */
-    void LoadMD5AnimFile ();
+    void LoadMD5AnimFile (const std::string& fileNameSuffix = "");
+
+    // -------------------------------------------------------------------
+    /** Load *.MD5ANIM files as configured.
+     */
+    void LoadConfiguredMD5AnimFiles ();
 
     // -------------------------------------------------------------------
     /** Load a *.MD5CAMERA file.
@@ -151,6 +157,8 @@ protected:
     */
     void LoadFileIntoMemory (IOStream* pFile);
     void UnloadFileFromMemory ();
+    
+    static std::string AppendFileSuffix (const std::string& nameWithDot, const std::string& suffix);
 
 
     /** IOSystem to be used to access files */
@@ -186,6 +194,11 @@ protected:
 
     /** configuration option: prevent anim autoload */
     bool configNoAutoLoad;
+    
+    /** configuration option: names of anim suffixes */
+    std::string configAnimSeqs;
+    
+    std::vector<aiAnimation *> mAnimations;
 };
 
 } // end of namespace Assimp
